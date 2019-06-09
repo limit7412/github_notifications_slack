00056ef3431bae2e72331fb0cdcb04e652db4afb
kairox <git@newgenerations.im>
2019-06-10 01:07:56 +0900

    debug wip

--- src/github_notifications_slack.cr
+++ src/github_notifications_slack.cr
@@ -6,19 +6,20 @@ module GithubNotificationsSlack
 
     main do
       version "version: #{VERSION}", short: "-v"
-      option "-t NAME", "--test=NAME",
-        type: String,
-        desc: "test.",
-        required: true
-      option "-b", "--bool-test",
-        type: Bool,
-        desc: "hoge."
+      # option "-t NAME", "--test=NAME",
+      #   type: String,
+      #   desc: "test.",
+      #   required: true
+      # option "-b", "--bool-test",
+      #   type: Bool,
+      #   desc: "hoge."
 
       run do |opts, args|
-        if opts.bool_test
-          puts "bool"
-        end
-        puts "#{opts.test}."
+        # if opts.bool_test
+        #   puts "bool"
+        # end
+        # puts "#{opts.test}."
+        puts "test"
       end
     end
   end
