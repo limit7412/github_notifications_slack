08946564e9af4431464216bfff56bd6adddd0e76
qazx7412 <git@newgenerations.im>
2020-07-08 23:53:11 +0900

    fix

--- src/repository/github.cr
+++ src/repository/github.cr
@@ -23,8 +23,8 @@ class Github
   def get_comment(subject : GithubSubject) : GithubComment
     url = !subject.latest_comment_url.blank? ? subject.latest_comment_url : subject.url
     Lambda.print_log "comment url: #{url}"
-    if url == ""
-      comment = GithubComment.from_json %({
+    if url.blank?
+      return GithubComment.from_json %({
         "user": {},
         "body": "no comments exist"
       })
